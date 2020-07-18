dc6b2460641ddac85d32e9d9920869a2e33b3488
NAO <zackzakumi62@outlook.jp>
2020-07-18 03:04:46 +0000

    add sample.php

---
+++ sample.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+       <meta charset="UTF-8"> 
+       <title>おみくじ</title>
+    </head>
+    <body>
+      <h1>おみくじ</h1>  
+      <p>おみくじの結果</p>
+      <form action="sample.php" method="POST">
+         <button type="submit" name="act" Value="draw">おみくじを引く！</button>
+      </form>
+    </body>
+</html>
